ade1d6e4c9b8e441c10f90f7df4e775db2427352
OsmanOdasi <osmanodasi@gmail.com>
2023-06-17 20:07:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/erismapp/activities/Dashboard.java
@@ -0,0 +1,61 @@
+package com.example.erismapp.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.erismapp.R;
+import com.example.erismapp.databinding.ActivityDashboardBinding;
+import com.example.erismapp.fragments.EmployeeFragment;
+import com.example.erismapp.fragments.PayoutsFragment;
+import com.example.erismapp.fragments.RetirementBenefitFragment;
+import com.example.erismapp.fragments.RetirementPlanFragment;
+
+public class Dashboard extends AppCompatActivity {
+
+    ActivityDashboardBinding dashboardBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        dashboardBinding = ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(dashboardBinding.getRoot());
+
+        replaceFragment(new EmployeeFragment());
+
+        selectingNavigationItems();
+
+    }
+
+    private void selectingNavigationItems() {
+
+        dashboardBinding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_menu_employee) {
+                replaceFragment(new EmployeeFragment());
+            } else if (itemId == R.id.nav_menu_retirement_benefit) {
+                replaceFragment(new RetirementBenefitFragment());
+            } else if (itemId == R.id.nav_menu_retirement_plan) {
+                replaceFragment(new RetirementPlanFragment());
+            } else if (itemId == R.id.nav_menu_payout) {
+                replaceFragment(new PayoutsFragment());
+            }
+
+            return true;
+
+        });
+
+    }
+
+    private void replaceFragment(Fragment mFragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.frame_layout, mFragment);
+        fragmentTransaction.commit();
+    }
+}
---
+++ app/src/main/java/com/example/erismapp/fragments/PayoutsFragment.java
@@ -0,0 +1,23 @@
+package com.example.erismapp.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.erismapp.R;
+
+public class PayoutsFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_payouts, container, false);
+    }
+}
