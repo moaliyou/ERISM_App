db6e1337895d234b9c3b65cc155e8c8da89848db
OsmanOdasi <osmanodasi@gmail.com>
2023-07-03 22:49:33 +0300

    Added Login screen/activity

---
+++ app/src/main/java/com/example/erismapp/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.erismapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.erismapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
