5c22e54dc944b9668131f6db5ed2218246c1723f
OsmanOdasi <osmanodasi@gmail.com>
2023-07-07 09:59:01 +0300

    Added Signup screen/activity and event

--- app/src/main/java/com/example/erismapp/activities/SignupActivity.java
+++ app/src/main/java/com/example/erismapp/activities/SignupActivity.java
@@ -9,21 +9,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.erismapp.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
     }
 
-    public void signup(View view) {
-        Intent mIntent = new Intent(LoginActivity.this, SignupActivity.class);
+    public void login(View view) {
+        Intent mIntent = new Intent(SignupActivity.this, LoginActivity.class);
         mIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(mIntent);
     }
 
-    public void startSystem(View view) {
-        Toast.makeText(this, "Incorrect username or password", Toast.LENGTH_SHORT).show();
+    public void registerUser(View view) {
+        Toast.makeText(this, "Registered successfully", Toast.LENGTH_SHORT).show();
     }
 }
