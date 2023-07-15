b933e2815be92694ff22bcf7d41ec012fad718f7
OsmanOdasi <osmanodasi@gmail.com>
2023-07-15 19:29:08 +0300

    Added short toast message method

--- app/src/main/java/com/example/erismapp/helpers/MyHelperClass.java
+++ app/src/main/java/com/example/erismapp/helpers/MyHelperClass.java
@@ -7,4 +7,7 @@ public class MyHelperClass {
     public static void showLongToastMessage(Context mContext, String message) {
         Toast.makeText(mContext, message, Toast.LENGTH_LONG).show();
     }
+    public static void showToastMessage(Context mContext, String message) {
+        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show();
+    }
 }
